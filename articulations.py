b744959dd990cf51e7d0824df9892cb349263b1c
bekirerenpeker <bekirerenpeker@gmail.com>
2026-05-21 07:57:43 +0300

    added images for readme

--- articulations.py
+++ articulations.py
@@ -244,7 +244,7 @@ def get_arch_data(contour, avg_spacing):
             if mse > 20.0:
                 return None
 
-        if abs(a) < 0.004:
+        if abs(a) < 0.0015:
             return None
         vertex_x = -b / (2 * a)
 
